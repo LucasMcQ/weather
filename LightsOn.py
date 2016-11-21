37bd3552fec594eb60c48e3bc0987da341eb12f5
LucasMcQ <lmcq12@email.arizona.edu>
2016-11-21 00:38:25 -0700

    Create LightsOn.py

---
+++ LightsOn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#import RPi.GPIO as GPIO
+
+#GPIO.setmode(GPIO.PCM)
+
+#GPIO.setwarnings(False)
+
+#GPIO.setup(17, GPIO.OUT)
+#GPIO.setup(27, GPIO.OUT)
+
+print "Temp is greater or equal to 100!"
+print "Lights on!"
+
+#GPIO.output(17, GPIO.HIGH)
+#GPIO.output(27, GPIO.HIGH)
+
+
+#GPIO.cleanup()
